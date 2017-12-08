02a40321ec700e86f898b9483fded392c266c1cb
usher.yue <usher.yue@gmail.com>
2017-12-08 13:46:29 +0800

    add

--- test/MiPushTest.js
+++ test/MiPushTest.js
@@ -7,5 +7,6 @@
  * 小米推送测试
  */
 const XiaoMi=require('../MiHelper');
+//将你从小米申请的信息写到这里
 XiaoMi.pushToAndroid('package','secret','1033145705652157622','title','payload');
 XiaoMi.pushToIos('bundleid','secret','1033145705652157622','title','payload');
