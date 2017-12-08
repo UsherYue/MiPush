3ab6ae4eafb21e0e8c7b3c973a4eb2af1e8fa1b0
usher.yue <usher.yue@gmail.com>
2017-12-08 10:32:43 +0800

    add package.json

--- test/MiPushTest.js
+++ test/MiPushTest.js
@@ -6,9 +6,9 @@
  * Time: 22:44
  * 小米推送测试
  */
-const Constants = require('./Constants');
-const Sender = require('./Sender');
-const IOSBuilder = require('./IOSBuilder');
+const Constants = require('../Constants');
+const Sender = require('../Sender');
+const IOSBuilder = require('../IOSBuilder');
 let test={
     /**
      * ios推送消息
@@ -24,6 +24,7 @@ let test={
         message.Badge('4');
         message.Extra('payload','qqqqqqqq');
         message.build();
+        //返回0 推送成功
         let errCode= (await sender.sendToUserAccount(message, '1033145705652157622')).getErrorCode();
         return errCode;
     },
